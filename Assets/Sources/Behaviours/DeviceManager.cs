c310ec126f0977514a0a0d3c4021591ec1c3ca00
Andrew Skuratov <skurat95@yandex.ru>
2018-04-19 13:53:11 +0300

    cardboard device supporting was added

---
+++ Assets/Sources/Behaviours/DeviceManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class DeviceManager : MonoBehaviour {
+
+	[SerializeField] private bool _enableCardboard = false;
+	
+	void Awake() {
+		if (_enableCardboard)
+			StartCoroutine(LoadDevice("Cardboard", true));
+	}
+
+	private IEnumerator LoadDevice(string deviceName, bool enable) {
+		VRSettings.LoadDeviceByName(deviceName);
+		yield return null;
+		VRSettings.enabled = enable;
+	}
+}
